7fa170f01fd2b3b37f3228c8353e0aebe705f132
Barun Parruck <barun.parruck@gmail.com>
2017-08-22 22:07:22 +0530

    Changed whitenoise and removed duplicate static files

--- bigyaan/settings.py
+++ bigyaan/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -125,7 +125,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'media'),
-]
+)
 SITE_ID = 1
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
